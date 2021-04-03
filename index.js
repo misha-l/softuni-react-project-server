d77f67bb84a3358c0b6aa9d1f7e88602a7402e24
misha-l <mkrusharska@gmail.com>
2021-04-03 18:35:51 +0300

    AtlasDB test4

--- index.js
+++ index.js
@@ -13,7 +13,18 @@ const fs = require("fs");
 // DB Setup
 // mongoose.connect("mongodb://127.0.0.1:27017/competition2");
 
-mongoose.connect("mongodb://127.0.0.1:27017/competition2");
+// mongoose.connect("mongodb://127.0.0.1:27017/competition2");
+
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    process.env.MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(" Mongoose is connected")
+  );
+} catch (e) {
+  console.log("could not connect");
+}
 
 // "mongodb+srv://MKrusharska:RdpSVcbmLFs57Rpi@mishacluster.2635z.mongodb.net/competition?retryWrites=true&w=majority"
 // "mongodb://MKrusharska:RdpSVcbmLFs57Rpi@mishacluster-shard-00-00.2635z.mongodb.net/competition?retryWrites=true&w=majority"
